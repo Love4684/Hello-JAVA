60231bcb3709798ac75b191826fc8e30deeca562
love4684 <lovekumar2017@iiitkottayam.ac.in>
2023-06-08 00:22:29 +0530

    k

---
+++ src/com/interview/Employee.java
@@ -0,0 +1,51 @@
+package com.interview;
+
+public class Employee {
+    private long empId;
+    private String empName;
+    private long salary;
+    private Gender gender;
+
+    public long getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(long empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                ", salary=" + salary +
+                ", gender=" + gender +
+                '}';
+    }
+}
